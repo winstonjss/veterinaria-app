ac19b3a0209f41122896f6d1b29cdc028c7d56ca
Rafael hoyos <Rafael rafinha77fup@gmail.com>
2024-10-10 21:52:48 -0500

    feat: Se crea la clase DocumentTypeDat

---
+++ WebAppVeterinaria/Data/DocumentTypeDat.cs
@@ -0,0 +1,133 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Data
+{
+    public class DocumentTypeDat
+    {
+        Persistence objPer = new Persistence();
+        //Metodo para mostrar todos los tipos de documento
+        public DataSet showDocumentType()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spSelectDocumentType";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+        //Metodo para mostrar unicamente el id y la descripcion
+        //public DataSet showCategoriesDDL()
+        //{
+        //    MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+        //    DataSet objData = new DataSet();
+
+        //    MySqlCommand objSelectCmd = new MySqlCommand();
+        //    objSelectCmd.Connection = objPer.openConnection();
+        //    objSelectCmd.CommandText = "spSelectCategoryDDL";
+        //    objSelectCmd.CommandType = CommandType.StoredProcedure;
+        //    objAdapter.SelectCommand = objSelectCmd;
+        //    objAdapter.Fill(objData);
+        //    objPer.closeConnection();
+        //    return objData;
+        //}
+
+        //Metodo para guardar un tipo de documento
+        public bool saveDocumentType(string _descripcion)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spInsertDocumentType"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_description", MySqlDbType.VarString).Value = _descripcion;
+
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+
+        }
+
+        //Metodo para actualizar un tipo de documento
+        public bool updatedocumenttype(int _id, string _descripcion)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spUpdateDocumentType"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("p_description", MySqlDbType.VarString).Value = _descripcion;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+
+        }
+
+        //Metodo para borrar un Tipo de documento
+        public bool deleteCategory(int _id)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spDeleteDocumentType"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+
+        }
+    }
+}

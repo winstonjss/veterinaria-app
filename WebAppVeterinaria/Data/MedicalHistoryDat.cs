4dc7069d976524063575605811d337e4e1f18b37
winstonjss <jhonatan_15-01@hotmail.com>
2024-10-10 23:21:48 -0500

    feat: se crea la clase MedicalHistoryDat.cs

---
+++ WebAppVeterinaria/Data/MedicalHistoryDat.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Data
+{
+    public class MedicalHistory
+    {
+        Persistence objPer = new Persistence();        
+        public DataSet showMedicalHistoryByAnimal(int _idAnimal)
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spSelectHistoriaClinicaAnimal";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objSelectCmd.Parameters.Add("p_anim_id", MySqlDbType.Int32).Value = _idAnimal;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
+        public bool saveMedicalHistoryByDateId(int _dateId)
+        {
+            bool executed = false;
+            int row;
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spInsertHistoriaClinica"; //nombre del proce dimiento almacenado 
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_cit_id", MySqlDbTy.Int32).Value = _dateId;
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        public bool saveMedicalHistoryByDate(DateTime _date)
+        {
+            bool executed = false;
+            int row;
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spInsertHistoriaClinicaPorFecha"; //nombre del proce dimiento almacenado 
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_fecha", MySqlDbTy.Int32).Value = _date;
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+    }
+}

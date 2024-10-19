d946715f7b2ff0a2e4d25df0ad7a5eee7410e38a
Rafael hoyos <Rafael rafinha77fup@gmail.com>
2024-10-19 12:23:39 -0500

    feat: se crea la clase RolesLog

---
+++ WebAppVeterinaria/Logic/RolesLog.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data;
+
+namespace Logic
+{
+    public class RolesLog
+    {
+        RolesDat objRol = new RolesDat();
+
+        //Metodo para mostrar todos los Roles
+        public DataSet showRoles()
+        {
+             return objRol.showRoles();
+        }
+
+        //Metodo para mostrar Unicamente el ID y el nombre del rol 
+        public DataSet showRolesDDL()
+        {
+            return objRol.showRolesDDL();
+        }
+
+        //Metodo para guardar un Rol
+        public bool saveRoles(string _nombre, string _descripcion)
+        {
+            return objRol.saveRoles(_nombre, _descripcion);
+
+        }
+
+        //Metodo para actualizar un Rol
+        public bool updateRol(int _id, string _nombre, string _descripcion)
+        {
+            return objRol.updateRol(_id, _nombre, _descripcion)
+        }
+
+        //Metodo para eliminar un Rol
+        public bool deletePermision(int _id)
+        {
+            return objRol.deletePermision(_id);
+        }
+    }

bc3593f90035e4c35214cf2ca4f38c4c3ae0ccd5
winstonjss <jhonatan_15-01@hotmail.com>
2024-11-01 01:15:12 -0500

    feat, se agrega WFMedicalHistory a la capa de presentacion

---
+++ WebAppVeterinaria/Presentation/WFMedicalHistory.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Logic;
+namespace Presentation
+{
+    public partial class WFMedicalHistory : System.Web.UI.Page
+    {
+        MedicalHistoryLog objMed = new MedicalHistoryLog();
+        AppointmentsLog objApp = new AppointmentsLog();
+        private int _fkAppoitment;
+        private bool executed = false;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                showAppoitmentsDDL();
+                showMedicalHistoryAll();
+            }
+        }
+
+        private void showMedicalHistoryAll()
+        {
+            DataSet ds = new DataSet();
+            ds = objMed.showMedicalHistoryAll();
+            GVHistoryMedical.DataSource = ds;
+            GVHistoryMedical.DataBind();
+        }
+
+        private void showAppoitmentsDDL()
+        {
+            DDLAppoitment.DataSource = objApp.showCitasDDl();
+            DDLAppoitment.DataValueField = "cit_id";
+            DDLAppoitment.DataTextField = "cit_fecha";
+            DDLAppoitment.DataBind();
+            DDLAppoitment.Items.Insert(0, "seleccione");
+
+        }
+
+        //eventos que se ejecutan cuando se da click en los botones
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _fkAppoitment = Convert.ToInt32(DDLAppoitment.SelectedValue);
+
+            executed = objMed.saveMedicalHistoryByDateId(_fkAppoitment);
+            if (executed)
+            {
+                lblMsg.Text = "se guardo la historia clinica";
+                showMedicalHistoryAll();
+            }
+            else
+            {
+                lblMsg.Text = "erorr al guardar";
+            }
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebAppVeterinaria/Presentation/WFMedicalHistory.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Presentation
+{
+
+
+    public partial class WFMedicalHistory
+    {
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control DDLAppoitment.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDLAppoitment;
+
+        /// <summary>
+        /// Control BtnSave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnSave;
+
+        /// <summary>
+        /// Control BtnUpdate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnUpdate;
+
+        /// <summary>
+        /// Control lblMsg.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMsg;
+
+        /// <summary>
+        /// Control GVHistoryMedical.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GVHistoryMedical;
+    }
+}

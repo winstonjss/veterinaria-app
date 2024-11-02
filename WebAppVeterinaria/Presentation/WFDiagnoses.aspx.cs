e3f2f5b01c35a51fba3b0079e9e904c9f3c95e79
unknown <estivenmendezr@gmail.com>
2024-11-02 00:13:04 -0500

    feat: se crea WFADiagnoses en la capa de presentacion

---
+++ WebAppVeterinaria/Presentation/WFDiagnoses.aspx.cs
@@ -0,0 +1,73 @@
+﻿using Logic;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Presentation
+{
+    public partial class WFDiagnoses : System.Web.UI.Page
+    {
+        AnamnesisLog objAnam = new AnamnesisLog();
+        DiagnosesLog objDiag = new DiagnosesLog();
+
+        private int _fkAnemnesis;
+        private string _clasification, _code;
+        private bool executed = false;
+
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                showDiagnosesAll();
+                showAnamnesisDDL();
+
+            }
+        }
+
+        private void showDiagnosesAll()
+        {
+            DataSet dataSet = new DataSet();
+            dataSet = objDiag.showDiagnosticos();
+            GVDiagnoses.DataSource = dataSet;
+            GVDiagnoses.DataBind();
+        }
+        private void showAnamnesisDDL()
+        {
+            DDLAnamnesis.DataSource = objAnam.showAnamnesisDDl();
+            DDLAnamnesis.DataValueField = "anam_id";
+            DDLAnamnesis.DataTextField = "anam_descripcion";
+            DDLAnamnesis.DataBind();
+            DDLAnamnesis.Items.Insert(0, "seleccione");
+        }
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _fkAnemnesis = Convert.ToInt32(DDLAnamnesis.SelectedValue);
+
+            _clasification = TBClasification.Text;
+            _code = TBCode.Text;
+            executed = objDiag.saveDiagnostico(_clasification,_code,_fkAnemnesis);
+            if (executed)
+            {
+                lblMsg.Text = "se guardo la historia clinica";
+                showDiagnosesAll();
+            }
+            else
+            {
+                lblMsg.Text = "erorr al guardar";
+            }
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebAppVeterinaria/Presentation/WFDiagnoses.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Presentation
+{
+
+
+    public partial class WFDiagnoses
+    {
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control TBClasification.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBClasification;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control TBCode.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBCode;
+
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Control DDLAnamnesis.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DDLAnamnesis;
+
+        /// <summary>
+        /// Control BtnSave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnSave;
+
+        /// <summary>
+        /// Control BtnUpdate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnUpdate;
+
+        /// <summary>
+        /// Control lblMsg.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMsg;
+
+        /// <summary>
+        /// Control GVDiagnoses.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GVDiagnoses;
+    }
+}

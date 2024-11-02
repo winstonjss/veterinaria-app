2e670e963f88eea7a4cec3ddd165f255b60a23bf
Rafael hoyos <Rafael rafinha77fup@gmail.com>
2024-11-01 21:34:54 -0500

    feat: Se crea la clase WFOffice en la capa de presentacion

---
+++ WebAppVeterinaria/Presentation/WFOffice.aspx.cs
@@ -0,0 +1,61 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Presentation
+{
+    public partial class WFOffice : System.Web.UI.Page
+    {
+        OfficeLog objOfi = new OfficeLog();
+        private int _id;
+        private string _num_consultorio;
+        private bool executed = false;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //aqui se invocan todos los métodos 
+                showOffice();
+
+            }
+        }
+
+        public void showOffice()
+        {
+            DataSet ds = new DataSet();
+            ds = objOfi.showOffice();
+            GVOffice.DataSource = ds;
+            GVOffice.DataBind();
+
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _num_consultorio = TBCon_num_consultorio.Text;
+            executed = objOfi.saveOffice(_num_consultorio);
+
+            if (executed)
+            {
+                LblMsg.Text = "Se guardó exitosamente ";
+                showOffice();
+                TBCon_num_consultorio.Text = "";
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar ";
+            }
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebAppVeterinaria/Presentation/WFOffice.aspx.designer.cs
@@ -0,0 +1,80 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Presentation
+{
+
+
+    public partial class WFOffice
+    {
+
+        /// <summary>
+        /// Control TBId.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBId;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control TBCon_num_consultorio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TBCon_num_consultorio;
+
+        /// <summary>
+        /// Control BtnSave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnSave;
+
+        /// <summary>
+        /// Control BtnUpdate.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnUpdate;
+
+        /// <summary>
+        /// Control LblMsg.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LblMsg;
+
+        /// <summary>
+        /// Control GVOffice.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GVOffice;
+    }
+}
